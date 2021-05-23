73d5dd7e36cd72043590c634f3a8e7458cd96c93
NET Stack & JS Frameworks <dtro-devuk@users.noreply.github.com>
2021-05-23 15:52:17 +0100

    clean

--- _next/static/aFA_0HJoQ4ie8Apa8OWpR/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fblog\u002Fcooking","\u002Fblog\u002Fbrewing","\u002Fblog\u002Fdeveloper","\u002Fblog\u002Fgardening","\u002Fblog\u002Fbrewing\u002F[slug]","\u002Fblog\u002Fcooking\u002F[slug]","\u002Fblog\u002Fgardening\u002F[slug]","\u002Fblog\u002Fdeveloper\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
